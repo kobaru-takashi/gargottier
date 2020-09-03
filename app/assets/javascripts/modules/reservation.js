0b125189907f767ddac493bb3255368226daf7e0
kobaru-takashi <ta.a.1445.1@gmail.com>
2020-09-04 00:19:15 +0900

    登録画面　完了

---
+++ app/assets/javascripts/modules/reservation.js
@@ -0,0 +1,44 @@
+$(function(){
+  $('#form-btn').on('click', function(){
+    if($('#reservation-time').val() === ''){
+      alert('ご来店時間を入力してください！');
+      $(this).focus();
+      return false;
+    }
+    if($('#number-of-people').val() === ''){
+      alert('ご来店人数を入力してください！');
+      $(this).focus();
+      return false;
+    }
+    if($('#family-name-btn').val() === ''){
+      alert('代表者名(姓)を入力してください！');
+      $(this).focus();
+      return false;
+    }
+    if($('#first-name-btn').val() === ''){
+      alert('代表者名(名)を入力してください！');
+      $(this).focus();
+      return false;
+    }
+    if($('#family-name-kana-btn').val() === ''){
+      alert('代表者名(セイ)を入力してください！');
+      $(this).focus();
+      return false;
+    }
+    if($('#first-name-kana-btn').val() === ''){
+      alert('代表者名(メイ)を入力してください！');
+      $(this).focus();
+      return false;
+    }
+    if($('#phone-btn').val() === ''){
+      alert('電話番号を入力してください！');
+      $(this).focus();
+      return false;
+    }
+    if($('#email-btn').val() === ''){
+      alert('メールアドレスを入力してください！');
+      $(this).focus();
+      return false;
+    }
+  });
+});
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,10 +2,14 @@ class ReservationsController < ApplicationController
 
   def index
     @events = Event.find(params[:event_id])
+    session["no_card"] = @reservation
+  end
+  def new
+    @events = Event.find(params[:event_id])
   end
 
-  private
-  def product_params
-    params.require(:product).permit(:reservation_time, :number_of_people)
-  end
+  # private
+  # def product_params
+  #   params.require(:product).permit(:reservation_time, :number_of_people)
+  # end
 end
