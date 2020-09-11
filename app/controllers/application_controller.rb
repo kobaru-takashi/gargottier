07ad390fa1424a6a09fc15b26a86b159e2498f29
kobaru-takashi <ta.a.1445.1@gmail.com>
2020-09-11 23:05:46 +0900

    調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_authentication
 
+  private
+
+  def basic_authentication
+    authenticate_or_request_with_http_basic do |name, password|
+      name == "Gargotier" && password == "Gargotier_password_0990"
+    end
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def basic_authentication
     authenticate_or_request_with_http_basic do |name, password|
-      name == "Gargotier" && password == "Gargotier_password"
+      name == "Admin" && password == "Admin_password_0880"
     end
   end
 end
