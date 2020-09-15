23111cd729e7b2da986d490c6401b64a5dddbf1a
kobaru-takashi <ta.a.1445.1@gmail.com>
2020-09-15 23:31:41 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_authentication
+  # before_action :basic_authentication
 
-  private
+  # private
 
-  def basic_authentication
-    authenticate_or_request_with_http_basic do |name, password|
-      name == Rails.application.credentials.basic_auth[:user] && password == Rails.application.credentials.basic_auth[:password]
-    end
-  end
+  # def basic_authentication
+  #   authenticate_or_request_with_http_basic do |name, password|
+  #     name == Rails.application.credentials.basic_auth[:user] && password == Rails.application.credentials.basic_auth[:password]
+  #   end
+  # end
 end
